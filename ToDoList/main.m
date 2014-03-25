8e13de79f5c9e1251b51763d701f46b1c369c3be
Transcend <transcend@Silverquick.local>
2014-03-24 22:03:26 -0400

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Transcend on 3/24/14.
+//  Copyright (c) 2014 Thinkful Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
